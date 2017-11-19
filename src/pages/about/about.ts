f0faa12201c8970624488b1324af1fa033259cbe
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-11-19 18:52:18 -0500

    Página de perfil

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -8,9 +8,9 @@ import { NavController } from 'ionic-angular';
 export class AboutPage {
 
   notifications: Array<Notification> = [
-    { id:1, action: 'reply', img: 'https://pbs.twimg.com/profile_images/849039710978625537/YTxV5qxk_400x400.jpg', nick_name: '@freddier', text: 'platzitter es lo mas' },
-    { id:2, action: 'reclonetwitt', img: 'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name: '@cvander', text: 'que chulo que esta esta app en iOS' },
-    { id:3, action: 'mention', img: 'https://pbs.twimg.com/profile_images/855547570323689473/I-fpilWj_400x400.jpg', nick_name: '@jorgeucano', text: 'test 1234' }
+    { id: 1, action: 'reply', img: 'https://pbs.twimg.com/profile_images/849039710978625537/YTxV5qxk_400x400.jpg', nick_name: '@freddier', text: 'platzitter es lo mas' },
+    { id: 2, action: 'reclonetwitt', img: 'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name: '@cvander', text: 'que chulo que esta esta app en iOS' },
+    { id: 3, action: 'mention', img: 'https://pbs.twimg.com/profile_images/855547570323689473/I-fpilWj_400x400.jpg', nick_name: '@jorgeucano', text: 'test 1234' }
   ];
 
   constructor(public navCtrl: NavController) {
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,8 +7,48 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+  profile:Profile = {
+    'img': 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg',
+    'nick_name': '@omiguelperez',
+    'interactions': 1,
+    'github': 'omiguelperez',
+    'twitter': '@omiguelperez',
+    'facebook': 'omiguelperez',
+    'bio': 'Oscar Pérez es un Ingeniero de Sistemas. Desarrollador de Software con Metodologías ágiles. BDD & TDD. Desarrollador Bakend con {JavaScript/Node.js}. Tiene conocimiento de Angular 2, Ionic 2, Django. En constante aprendizaje.'
+  };
+
+  notifications: Array<Notification> = [
+    { id: 1, action: 'reply', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omgieuleprez', text: 'platzitter es lo mas' },
+    { id: 2, action: 'reclonetwitt', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omiguelperez', text: 'que chulo que esta esta app en iOS' },
+    { id: 3, action: 'mention', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omigelperez', text: 'test 1234' },
+    { id: 1, action: 'reply', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omgieuleprez', text: 'platzitter es lo mas' },
+    { id: 2, action: 'reclonetwitt', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omiguelperez', text: 'que chulo que esta esta app en iOS' },
+    { id: 3, action: 'mention', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omigelperez', text: 'test 1234' },
+    { id: 1, action: 'reply', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omgieuleprez', text: 'platzitter es lo mas' },
+    { id: 2, action: 'reclonetwitt', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omiguelperez', text: 'que chulo que esta esta app en iOS' },
+    { id: 3, action: 'mention', img: 'https://pbs.twimg.com/profile_images/903694094475685888/5_piqAOV_400x400.jpg', nick_name: '@omigelperez', text: 'test 1234' }
+  ];
+  
   constructor(public navCtrl: NavController) {
 
   }
 
+}
+
+interface Profile {
+  img: string;
+  nick_name: string;
+  interactions: number;
+  github: string;
+  twitter: string;
+  facebook: string;
+  bio: string;
+}
+
+interface Notification {
+  id: number;
+  action: string;
+  img: string;
+  nick_name: string;
+  text: string;
 }
