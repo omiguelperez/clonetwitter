9f189d2e4299ca430ae1cf75084224810da7807c
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-11-19 17:47:10 -0500

    Home page: Listado de twitts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  listado:Array<Twitt> = [
+    { img: 'https://pbs.twimg.com/profile_images/849039710978625537/YTxV5qxk_400x400.jpg', nick_name: '@freddier', text: 'platzitter es lo mas' },
+    { img: 'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name: '@cvander', text: 'que chulo que esta esta app en iOS' },
+    { img: 'https://pbs.twimg.com/profile_images/855547570323689473/I-fpilWj_400x400.jpg', nick_name: '@jorgeucano', text: 'test 1234' }
+  ];
+  
   constructor(public navCtrl: NavController) {
 
   }
 
+}
+
+interface Twitt {
+  img: string;
+  nick_name: string;
+  text: string;
 }
